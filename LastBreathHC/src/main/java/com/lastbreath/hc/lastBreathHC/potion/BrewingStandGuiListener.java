03eeddf30552feda83cfd2e4a49f716b06d9dc3f
Lethal <47657372+TotallyNotLethal@users.noreply.github.com>
2026-01-20 08:26:38 -0500

    Refactor inventory event imports in BrewingStandGuiListener
    Consolidate inventory event imports for clarity.

--- LastBreathHC/src/main/java/com/lastbreath/hc/lastBreathHC/potion/BrewingStandGuiListener.java
+++ LastBreathHC/src/main/java/com/lastbreath/hc/lastBreathHC/potion/BrewingStandGuiListener.java
@@ -9,15 +9,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.inventory.BrewEvent;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.*;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.BrewerInventory;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
-import org.bukkit.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
