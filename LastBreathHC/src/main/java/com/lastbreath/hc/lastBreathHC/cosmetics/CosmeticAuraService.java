992204e1c3bfcfc22ba9c49f94e918332c0552d0
Lethal <47657372+TotallyNotLethal@users.noreply.github.com>
2026-03-05 11:50:31 -0500

    Fix Dragon Veil aura and make worldboss portals reliable

--- LastBreathHC/src/main/java/com/lastbreath/hc/lastBreathHC/cosmetics/CosmeticAuraService.java
+++ LastBreathHC/src/main/java/com/lastbreath/hc/lastBreathHC/cosmetics/CosmeticAuraService.java
@@ -3,6 +3,8 @@ package com.lastbreath.hc.lastBreathHC.cosmetics;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -24,10 +26,42 @@ public class CosmeticAuraService {
                     }
                     Location center = player.getLocation().clone().add(0, 1.0, 0);
                     Particle particle = aura.particle();
-                    player.getWorld().spawnParticle(particle, center, 6, 0.3, 0.4, 0.3, 0.02);
+                    spawnAuraParticle(player, center, particle);
                 }
             }
         }.runTaskTimer(plugin, 20L, 20L);
+    }
+
+    private void spawnAuraParticle(Player player, Location center, Particle particle) {
+        Class<?> dataType = particle.getDataType();
+        if (Void.class.equals(dataType)) {
+            player.getWorld().spawnParticle(particle, center, 6, 0.3, 0.4, 0.3, 0.02);
+            return;
+        }
+
+        if (Float.class.equals(dataType)) {
+            player.getWorld().spawnParticle(particle, center, 6, 0.3, 0.4, 0.3, 0.02, 1.0f);
+            return;
+        }
+
+        if (Integer.class.equals(dataType)) {
+            player.getWorld().spawnParticle(particle, center, 6, 0.3, 0.4, 0.3, 0.02, 1);
+            return;
+        }
+
+        if (ItemStack.class.equals(dataType)) {
+            player.getWorld().spawnParticle(particle, center, 6, 0.3, 0.4, 0.3, 0.02,
+                    new ItemStack(org.bukkit.Material.STONE));
+            return;
+        }
+
+        if (BlockData.class.equals(dataType)) {
+            player.getWorld().spawnParticle(particle, center, 6, 0.3, 0.4, 0.3, 0.02,
+                    org.bukkit.Material.STONE.createBlockData());
+            return;
+        }
+
+        player.getWorld().spawnParticle(Particle.ENCHANT, center, 6, 0.3, 0.4, 0.3, 0.02);
     }
 
     public void stop() {
